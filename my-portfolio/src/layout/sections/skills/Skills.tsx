462dd1de9a65a46b3031ae803fd92d543301d418
Olga Dubavets <o.dubavets@gmail.com>
2024-09-25 23:48:54 +0200

    Added global styles

--- my-portfolio/src/layout/sections/skills/Skills.tsx
+++ my-portfolio/src/layout/sections/skills/Skills.tsx
@@ -1,12 +1,13 @@
 import React  from "react";
 import { Container } from "../../../components/Container";
 import { S } from "./Skills_Styles"
+import { SectionTitle } from "../../../components/SectionTitle";
 
 export const Skills: React.FC = () => {
     return (
         <S.Skills>
             <Container>
-                my skills
+                <SectionTitle>My Skills</SectionTitle>
             </Container>
         </S.Skills>
     );
