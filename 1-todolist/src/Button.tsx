01a05ce6709e8842350375c5df334adabc25372a
Olga Dubavets <o.dubavets@gmail.com>
2024-10-11 11:07:57 +0200

    Sync

--- 1-todolist/src/Button.tsx
+++ 1-todolist/src/Button.tsx
@@ -1,7 +1,9 @@
 type ButtonPropsType = {
-    title: string
+	title: string
 }
 
-export const Button = ({ title }: ButtonPropsType) => {
-    return <button>{title}</button>
+export const Button = ({title}: ButtonPropsType) => {
+	return (
+		<button>{title}</button>
+	)
 }
